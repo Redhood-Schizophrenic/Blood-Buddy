a159e8948a7ec5af89f750775d94e9bc6cbca415
sahaskamble <sahaskamble282@gmail.com>
2024-05-17 04:11:39 +0530

    Minor bugs Fixed

--- src/app/components/donate/content.js
+++ src/app/components/donate/content.js
@@ -6,96 +6,96 @@ const Donate_Main = () =>
         <>
             <div className='relative w-[100%] h-auto pb-16'>
                 <div className="pt-16 flex justify-center">
-                    <table class="w-[60%] font-sans shadow-2xl text-lg text-left rtl:text-right text-white bg-white">
+                    <table className="w-[60%] font-sans shadow-2xl text-lg text-left rtl:text-right text-white bg-white">
                         <tbody>
                                 
-                                <tr class="bg-red-500 font-serif duration-500 text-3xl font-black">
-                                    <th scope="row" class="px-6 py-4 text-white whitespace-nowrap">
+                                <tr className="bg-red-500 font-serif duration-500 text-3xl font-black">
+                                    <th scope="row" className="px-6 py-4 text-white whitespace-nowrap">
                                     
                                     </th>
-                                    <td class="px-6 py-4">
+                                    <td className="px-6 py-4">
                                         Connect with us & save lives around your location
                                     </td>
                                     
                                 </tr>
 
-                                <tr class="duration-500 bg-white text-black font-bold hover:scale-110 text-2xl hover:bg-pink-200">
-                                    <th scope="row" class="px-6 py-4 font-normal text-white whitespace-nowrap">
+                                <tr className="duration-500 bg-white text-black font-bold hover:scale-110 text-2xl hover:bg-pink-200">
+                                    <th scope="row" className="px-6 py-4 font-normal text-white whitespace-nowrap">
                                         <div className="bg-pink-400 flex justify-center rounded-full w-[35px] h-[35px]">
                                             1
                                         </div>
                                     </th>
-                                    <td class="px-6 py-4">
+                                    <td className="px-6 py-4">
                                         Register 🧍🏻
                                     </td>
                                     
                                 </tr>
 
-                                <tr class="duration-500 bg-white text-black font-bold hover:scale-110 text-2xl hover:bg-pink-200">
-                                    <th scope="row" class="px-6 py-4 font-normal text-white whitespace-nowrap">
+                                <tr className="duration-500 bg-white text-black font-bold hover:scale-110 text-2xl hover:bg-pink-200">
+                                    <th scope="row" className="px-6 py-4 font-normal text-white whitespace-nowrap">
                                         <div className="bg-pink-400 flex justify-center rounded-full w-[35px] h-[35px]">
                                             2
                                         </div>
                                     </th>
-                                    <td class="px-6 py-4">
+                                    <td className="px-6 py-4">
                                         Post a Blood request 🩸
                                     </td>
                                     
                                 </tr>
                                     
-                                <tr class="duration-500 bg-white text-black font-bold hover:scale-110 text-2xl hover:bg-pink-200">
-                                    <th scope="row" class="px-6 py-4 font-normal text-white whitespace-nowrap">
+                                <tr className="duration-500 bg-white text-black font-bold hover:scale-110 text-2xl hover:bg-pink-200">
+                                    <th scope="row" className="px-6 py-4 font-normal text-white whitespace-nowrap">
                                         <div className="bg-pink-400 flex justify-center rounded-full w-[35px] h-[35px]">
                                             3
                                         </div>
                                     </th>
-                                    <td class="px-6 py-4">
+                                    <td className="px-6 py-4">
                                         Respond ✅
                                     </td>
                                     
                                 </tr>
 
-                                <tr class="duration-500 bg-white text-black font-bold hover:scale-110 text-2xl hover:bg-pink-200">
-                                    <th scope="row" class="px-6 py-4 font-normal text-white whitespace-nowrap">
+                                <tr className="duration-500 bg-white text-black font-bold hover:scale-110 text-2xl hover:bg-pink-200">
+                                    <th scope="row" className="px-6 py-4 font-normal text-white whitespace-nowrap">
                                         <div className="bg-pink-400 flex justify-center rounded-full w-[35px] h-[35px]">
                                             4
                                         </div>
                                     </th>
-                                    <td class="px-6 py-4">
+                                    <td className="px-6 py-4">
                                         Get notified 📱
                                     </td>
                                     
                                 </tr>
 
-                                <tr class="duration-500 bg-white text-black font-bold hover:scale-110 text-2xl hover:bg-pink-200">
-                                    <th scope="row" class="px-6 py-4 font-normal text-white whitespace-nowrap">
+                                <tr className="duration-500 bg-white text-black font-bold hover:scale-110 text-2xl hover:bg-pink-200">
+                                    <th scope="row" className="px-6 py-4 font-normal text-white whitespace-nowrap">
                                         <div className="bg-pink-400 flex justify-center rounded-full w-[35px] h-[35px]">
                                             5
                                         </div>
                                     </th>
-                                    <td class="px-6 py-4">
+                                    <td className="px-6 py-4">
                                         Forever Free 💲❌
                                     </td>
                                     
                                 </tr>
 
-                                <tr class="duration-500 bg-white text-black font-bold hover:scale-110 text-2xl hover:bg-pink-200">
-                                    <th scope="row" class="px-6 py-4 font-normal text-white whitespace-nowrap">
+                                <tr className="duration-500 bg-white text-black font-bold hover:scale-110 text-2xl hover:bg-pink-200">
+                                    <th scope="row" className="px-6 py-4 font-normal text-white whitespace-nowrap">
                                         <div className="bg-pink-400 flex justify-center rounded-full w-[35px] h-[35px]">
                                             6
                                         </div>
                                     </th>
-                                    <td class="px-6 py-4">
+                                    <td className="px-6 py-4">
                                         Save a Life 🩸🧬
                                     </td>
                                     
                                 </tr>
 
-                                <tr class="bg-white font-serif duration-500 text-3xl font-black">
-                                    <th scope="row" class="px-6 py-4 text-white whitespace-nowrap">
+                                <tr className="bg-white font-serif duration-500 text-3xl font-black">
+                                    <th scope="row" className="px-6 py-4 text-white whitespace-nowrap">
                                     
                                     </th>
-                                    <td class="px-6 py-4">
+                                    <td className="px-6 py-4">
                                         <div className="flex text-black justify-center">
                                             <a href="/register" className="-ml-12 bg-red-500 text-white px-2 py-1 rounded hover:scale-110 duration-500">
                                                 Register
